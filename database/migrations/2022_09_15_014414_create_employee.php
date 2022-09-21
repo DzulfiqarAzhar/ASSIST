604ca597210f635821c6f65891ba574903f890ee
Dzulfiqar Azhar <abang.fiqarz@gmail.com>
2022-09-21 13:13:33 +0700

    first commit

---
+++ database/migrations/2022_09_15_014414_create_employee.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->integer('emp_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('place_of_birth');
+            $table->text('address');
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+};
